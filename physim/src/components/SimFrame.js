321e3ef18ca6f2f3f27933be187634a21133e963
fabioc9675 <fabioc9675@gmail.com>
2021-08-19 18:13:19 -0500

    Creation of PhySim project as a main viewport of simulations

---
+++ physim/src/components/SimFrame.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SimFrame() {
+  return 'Hello World!'
+}
+
+export default SimFrame;
---
+++ physim/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SimFrame from './components/SimFrame';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SimFrame />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
