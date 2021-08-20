b7389c85470bb4e565d26f6595b3202451d6da92
fabioc9675 <fabioc9675@gmail.com>
2021-08-19 20:19:42 -0500

    Creation of Editor component

--- physim/src/components/SimFrame.js
+++ physim/src/components/SimFrame.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+// local imports
+import Editor from "./Editor";
 
 function SimFrame() {
-  return 'Hello World!'
+  // definition of language Used in simulator
+  const [p5, setP5] = useState('')
+
+
+  return (
+  <>
+    <div className= "pane top-left-pane">
+      <Editor
+        language = 'javascript'
+        displayName = "Editor de código"
+        value = {p5}
+        onChange = {setP5} 
+      />
+    </div>
+  </>
+  )
 }
 
 export default SimFrame;
