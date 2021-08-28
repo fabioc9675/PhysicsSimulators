0219291fbf3770ec0d6351a48c45ebad71a0090b
Fabian Castaño <fabioc9675@gmail.com>
2021-08-27 21:48:55 -0500

    Ceation of component to access at the simulations

--- physim/src/components/MainView.js
+++ physim/src/components/MainView.js
@@ -22,27 +22,120 @@ export default function MainView() {
         <div className="MainFrame-pane pane-left">
           <MainInfo />
         </div>
-        <div>
-          <div className="SimButton">
-            <SimDescriptor
-              buttonLabel="Simulacion 1"
-              pathUrl={dataSim.sim01.path}
-              imgDir={dataSim.sim01.icon}
-            />
+        <div className="SimFrame">
+          <div className="Simulators-pane">
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 1"
+                pathUrl={dataSim.sim01.path}
+                imgDir={dataSim.sim01.icon}
+                title={dataSim.sim01.title}
+                abstract={dataSim.sim01.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 2"
+                pathUrl={dataSim.sim02.path}
+                imgDir={dataSim.sim02.icon}
+                title={dataSim.sim02.title}
+                abstract={dataSim.sim02.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 3"
+                pathUrl={dataSim.sim03.path}
+                imgDir={dataSim.sim03.icon}
+                title={dataSim.sim03.title}
+                abstract={dataSim.sim03.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 1"
+                pathUrl={dataSim.sim01.path}
+                imgDir={dataSim.sim01.icon}
+                title={dataSim.sim01.title}
+                abstract={dataSim.sim01.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 2"
+                pathUrl={dataSim.sim02.path}
+                imgDir={dataSim.sim02.icon}
+                title={dataSim.sim02.title}
+                abstract={dataSim.sim02.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 3"
+                pathUrl={dataSim.sim03.path}
+                imgDir={dataSim.sim03.icon}
+                title={dataSim.sim03.title}
+                abstract={dataSim.sim03.abst}
+              />
+            </div>
           </div>
-          <div className="SimButton">
-            <SimDescriptor
-              buttonLabel="Simulacion 2"
-              pathUrl={dataSim.sim02.path}
-              imgDir={dataSim.sim02.icon}
-            />
-          </div>
-          <div className="SimButton">
-            <SimDescriptor
-              buttonLabel="Simulacion 3"
-              pathUrl={dataSim.sim03.path}
-              imgDir={dataSim.sim03.icon}
-            />
+        </div>
+        <div className="SimFrame">
+          <div className="Simulators-pane">
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 1"
+                pathUrl={dataSim.sim01.path}
+                imgDir={dataSim.sim01.icon}
+                title={dataSim.sim01.title}
+                abstract={dataSim.sim01.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 2"
+                pathUrl={dataSim.sim02.path}
+                imgDir={dataSim.sim02.icon}
+                title={dataSim.sim02.title}
+                abstract={dataSim.sim02.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 3"
+                pathUrl={dataSim.sim03.path}
+                imgDir={dataSim.sim03.icon}
+                title={dataSim.sim03.title}
+                abstract={dataSim.sim03.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 1"
+                pathUrl={dataSim.sim01.path}
+                imgDir={dataSim.sim01.icon}
+                title={dataSim.sim01.title}
+                abstract={dataSim.sim01.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 2"
+                pathUrl={dataSim.sim02.path}
+                imgDir={dataSim.sim02.icon}
+                title={dataSim.sim02.title}
+                abstract={dataSim.sim02.abst}
+              />
+            </div>
+            <div className="SimButton">
+              <SimDescriptor
+                buttonLabel="Simulacion 3"
+                pathUrl={dataSim.sim03.path}
+                imgDir={dataSim.sim03.icon}
+                title={dataSim.sim03.title}
+                abstract={dataSim.sim03.abst}
+              />
+            </div>
           </div>
         </div>
       </div>
--- physim/src/components/SimDescriptor.js
+++ physim/src/components/SimDescriptor.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { useHistory } from "react-router";
+import "../styles/SimDescriptor.css";
 
 export default function SimDescriptor(props) {
   // component props
-  const { buttonLabel, pathUrl, imgDir } = props;
+  const { buttonLabel, pathUrl, imgDir, title, abstract } = props;
 
   // URL history
   let history = useHistory();
@@ -14,9 +15,14 @@ export default function SimDescriptor(props) {
   }
 
   return (
-    <div>
-      <button onClick={HandleClick}>
-        <img src={imgDir} alt={buttonLabel} height="100px" />
+    <div className="desc-pane" onClick={HandleClick}>
+      <div className="des-pane des-content">
+        <div className="des-pane des-title">{title}</div>
+        <hr className="desc-line" />
+        <div className="des-pane des-abstract">{abstract}</div>
+      </div>
+      <button className="desc-pane desc-button">
+        <img className="des-pane desc-icon" src={imgDir} alt={buttonLabel} />
       </button>
     </div>
   );
