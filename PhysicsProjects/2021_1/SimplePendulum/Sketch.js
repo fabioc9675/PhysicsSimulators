f15b89e4807d9783aea74dafde1eb64d636a6daf
fabioc9675 <fabioc9675@gmail.com>
2021-10-01 15:26:31 -0500

    Modifications in visual distribution of SimplePendulum simulation

--- PhysicsProjects/2021_1/SimplePendulum/Sketch.js
+++ PhysicsProjects/2021_1/SimplePendulum/Sketch.js
@@ -36,39 +36,51 @@ let tmax_datos = 120;
 let m0,L0,g0,theta_0,omega_0;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(820, 366);
   frameRate(60);
   
   theta0 = 30;
   omega0 = 0;
   
   m = createSlider(2, 10, 6);
-  m.position(215, 10);
-  l = createSlider(1, 250, 150);
-  l.position(215, 35);
+  l = createSlider(30, 230, 150);
   g = createSlider(1, 20, 9.8, 0.1);
-  g.position(215, 60);
-  theta00 = createSlider(0, 360, theta0, 1);
+  theta00 = createSlider(-180, 180, theta0, 1);
   omega00 = createSlider(-3, 3, omega0, 0.5);
   
   timer = createP("timer");
   setInterval(timeIt, 1000);
-  timer.position(65, 70);
+  timer.position(80, 30);
   timer.style('font-size','25px');
+  
+  let masa = createDiv();
+  masa.style('transform: rotate(' + (270) + 'deg);');
+  masa.position(-30, 220);
+  masa.child(m);
+  
+  let longitud = createDiv();
+  longitud.style('transform: rotate(' + (270) + 'deg);');
+  longitud.position(25, 220);
+  longitud.child(l);
+  
+  let gravedad = createDiv();
+  gravedad.style('transform: rotate(' + (270) + 'deg);');
+  gravedad.position(80, 220);
+  gravedad.child(g);
   
   let d = createDiv();
   d.style('transform: rotate(' + (270) + 'deg);');
-  d.position(460, 200);
+  d.position(620, 190);
   d.child(theta00);
   
   let f = createDiv();
   f.style('transform: rotate(' + (270) + 'deg);');
-  f.position(500, 200);
+  f.position(670, 190);
   f.child(omega00);
   
   boton_aplicar = createButton('Aplicar');
   boton_aplicar.mousePressed(cambios);
-  boton_aplicar.position(533, 340);
+  boton_aplicar.position(697, 330);
 
   theta0 *= PI/180
   theta = theta0; // Angulos
@@ -79,52 +91,53 @@ function setup() {
   
   boton_play = createButton('Play');
   boton_play.mousePressed(seguir);
-  boton_play.position(10, 10);
+  boton_play.position(20, 10);
   
   boton_pause = createButton('Pause');
   boton_pause.mousePressed(pausa);
-  boton_pause.position(60, 10);
+  boton_pause.position(75, 10);
   
   boton_reset = createButton('Reset');
   boton_reset.mousePressed(inicial);
-  boton_reset.position(120, 10);
+  boton_reset.position(140, 10);
   
   boton_MAS = createButton('Oscilador armonico');
   boton_MAS.style('background-color', color(255,50,60));
   boton_MAS.mousePressed(oscilador);
-  boton_MAS.position(450, 10);
+  boton_MAS.position(660, 10);
   
   boton_pendulo = createButton('NO pendulo real');
   boton_pendulo.style('background-color', color(0,255,0));
   boton_pendulo.mousePressed(pendulo);
-  boton_pendulo.position(468, 50);
+  boton_pendulo.position(670, 50);
   
   boton_guardar = createButton('Iniciar registro');
   boton_guardar.mousePressed(guardar);
-  boton_guardar.position(40, 50);
+  boton_guardar.position(210, 10);
 }
 
 function draw() {
   background(255);
 
   fill(0);
-  text('Angulo', 520, 120);
-  text(theta00.value(), 535, 320);
-  text('w', 575, 120);
-  text(omega00.value(), 577, 320);
-  text('m', 195, 25);
-  text(m.value() + ' kg', 385, 25);
-  text('L', 195, 50);
-  text(l.value() + ' cm', 385, 50);
-  text('g', 195, 75);
-  text(g.value() + ' m/s2', 385, 75);
+  text('Angulo', 680, 110);
+  text(theta00.value(), 692, 310);
+  text('w', 745, 110);
+  text(omega00.value(), 745, 310);
+  text('m [kg]', 35, 135);
+  text(m.value(), 44, 340);
+  text('L [cm]', 89, 135);
+  text(l.value(), 94, 340);
+  text('g [m/s2]', 140, 135);
+  text(g.value(), 150, 340);
   
-  translate(width/2-30, height/2);
+  translate(width/2+20, height/2-30);
 
   if (penduloIsOn){
     x1 = l.value()*sin(theta);
     y1 = l.value()*cos(theta);
 
+    strokeWeight(2);
     line(0, 0, x1, y1);
     fill(0, 255, 0);
     ellipse(x1, y1, m.value()*10, m.value()*10);
@@ -144,6 +157,7 @@ function draw() {
     y2 = l.value()*cos(theta_MAS);
     t += h;
     
+    strokeWeight(2);
     line(0, 0, x2, y2);
     fill(255, 0, 0);
     ellipse(x2, y2, m.value()*10, m.value()*10);
@@ -154,21 +168,22 @@ function draw() {
 
 function inicial(){
   clear();
-  createCanvas(600, 600);
+  createCanvas(820, 366);
   background(255);
   
   fill(0);
-  text('Angulo', 520, 120);
-  text(theta00.value(), 535, 320);
-  text('w', 575, 120);
-  text(omega00.value(), 577, 320);
-  text('m', 195, 25);
-  text(m.value() + ' kg', 385, 25);
-  text('L', 195, 50);
-  text(l.value() + ' cm', 385, 50);
-  text('g', 195, 75);
-  text(g.value() + ' m/s2', 385, 75);
-  translate(width/2-30, height/2);
+  text('Angulo', 680, 110);
+  text(theta00.value(), 692, 310);
+  text('w', 745, 110);
+  text(omega00.value(), 745, 310);
+  text('m [kg]', 35, 135);
+  text(m.value(), 44, 340);
+  text('L [cm]', 89, 135);
+  text(l.value(), 94, 340);
+  text('g [m/s2]', 140, 135);
+  text(g.value(), 150, 340);
+  
+  translate(width/2+20, height/2-30);
   
   theta = theta0; // Angulos
   omega = omega0; // Velocidades angulares
@@ -178,6 +193,7 @@ function inicial(){
   x1 = l.value()*sin(theta);
   y1 = l.value()*cos(theta);
 
+  strokeWeight(2);
   line(0, 0, x1, y1);
   fill(0, 255, 0);
   ellipse(x1, y1, m.value()*10, m.value()*10);
@@ -190,6 +206,7 @@ function inicial(){
     x2 = l.value()*sin(theta_MAS);
     y2 = l.value()*cos(theta_MAS);
     
+    strokeWeight(2);
     line(0, 0, x2, y2);
     fill(255, 0, 0);
     ellipse(x2, y2, m.value()*10, m.value()*10);
