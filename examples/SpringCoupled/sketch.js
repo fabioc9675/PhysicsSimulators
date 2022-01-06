086b802cc6c83460b20d7c22757fde4b57cefa0a
Fabian Castaño <fabioc9675@gmail.com>
2022-01-05 19:16:56 -0500

    Spring simulation working without WEBGL

--- examples/SpringCoupled/sketch.js
+++ examples/SpringCoupled/sketch.js
@@ -9,11 +9,14 @@ let reso2;
 
 let img;
 function preload() {
-  img = loadImage("coil.png");
+  //img = loadImage("coil.png");
+  img = loadImage(
+    "https://raw.githubusercontent.com/fabioc9675/PhysicsSimulators/devFabian/examples/SpringCoupled/coil.png"
+  );
 }
 
 function setup() {
-  createCanvas(800, 600, WEBGL);
+  createCanvas(800, 600);
   frameRate(30);
   cubo = new masa(60, 20, createVector(0, 0));
   wall = new Earth(createVector(-400, -300), 100, 600, 0);
@@ -23,7 +26,7 @@ function setup() {
 }
 
 function draw() {
-  // translate(800 / 2, 600 / 2);
+  translate(800 / 2, 600 / 2);
   background(0, 55, 65);
   wall.update();
   wall.show();
@@ -41,9 +44,9 @@ let spring = function (_k, _d) {
   this.drawSpring = function (Earth, masa) {
     fill(150, 40, 100);
     let baseWidth = 0.05 * (Earth.pos.x + Earth.size - masa.pos.x) + 25;
-    console.log(baseWidth);
-    texture(img);
-    rect(
+    //console.log(baseWidth);
+
+    image(img,
       Earth.pos.x + Earth.size,
       20 - baseWidth,
       abs(Earth.pos.x + Earth.size - masa.pos.x),
